2d4ee1e24437456fd6ffe5f8edaed9d4e6a8c5ef
Rohan Singh <rohannsingh98765@gmail.com>
2022-05-26 00:04:04 +0530

    Initiallisation
    Signed-off-by: Rohan Singh <rohannsingh98765@gmail.com>

---
+++ hooks/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import UseEffect from './components/UseEffect';
+import UseReducer from './components/UseReducer';
+import UseState from './components/UseState';
+
+function App() {
+  return (
+    <div className="App">
+
+      {/* <UseState /> */}
+
+      {/* <UseReducer /> */}
+
+      <UseEffect />
+
+    </div>
+  );
+}
+
+export default App;
